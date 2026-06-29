42e6e9a75cefbde566dbc17bc62bdb016e8a6549
Navya <navya3989@gmail.com>
2026-06-29 23:41:24 +0530

    Add: Updating day 1 java core programming codes

---
+++ src/javacoreprogramming/Q3.java
@@ -0,0 +1,15 @@
+package javacoreprogramming;
+
+public class Q3 {
+    static String s = "Java";
+    static String b = "Java";
+
+    public static void main(String[] args) {
+        if(s.equals(b)){
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+
+}
