a4d54733fea1fd455e65a0cdb9810de7e250c320
Navya <navya3989@gmail.com>
2026-06-30 00:10:11 +0530

    Add: Adding the array implementation codes

---
+++ src/array/FrequencyOfElement.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class FrequencyOfElement {
+    static int frequency = 0;
+    public static void main(String[] args) {
+        String[] num= {"1", "2", "3", "1", "2", "3", "2"};
+        for(int i =0; i<num.length; i++){
+            for(int j=0; j<num.length; j++){
+                if(num[i]==num[j]){
+                    frequency++;
+                }
+            }
+            System.out.println("Frequency of " + num[i] + " = " + frequency);
+            frequency =0;
+        }
+    }
+}
---
+++ src/array/reverse.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class reverse {
+    public static void main(String[] args) {
+        String[] alpha = {"ab", "bc", "cd", "de"};
+
+        for(int i=alpha.length -1; i>=0; i--){
+            System.out.println(alpha[i]);
+        }
+    }
+}
