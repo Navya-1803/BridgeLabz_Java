ce251ce61ec8a937fce82c30af85a8265d8247d4
Navya <navya3989@gmail.com>
2026-06-30 11:37:00 +0530

    Refactor: Removed unrelated packages

--- src/array/level1/PrintElement.java
+++
@@ -1,9 +0,0 @@
-package Array;
-
-public class PrintElement {
-    public static void main(String[] args) {
-        String[] arr = {"a", "b", "c", "d"};
-        System.out.println(arr[0]);
-        System.out.println(arr[3]);
-    }
-}
